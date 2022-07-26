0cf53381d8821305685ea069bf8a6ffa73f4e858
pacomantell <109753909+pacomantell@users.noreply.github.com>
2022-07-26 14:00:53 +0200

    Add files via upload

---
+++ Python/Tema7/Ejercicio1/calculadora.py
@@ -0,0 +1,13 @@
+#Módulo con las funciones basicas de una calculadora
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicar(a , b):
+    return a * b
+
+def dividir(a, b):
+    return a / b
---
+++ Python/Tema7/Ejercicio1/main.py
@@ -0,0 +1,8 @@
+import calculadora as calc
+x = float(2)
+y = float(5)
+print('Numeros:',(x,y))
+print('Suma(x+y): ', calc.suma(x,y))
+print('Resta(x-y): ', calc.resta(x,y))
+print('Multiplicacion(x*y): ', calc.multiplicar(x,y))
+print('Division(x/y):',calc.dividir(x,y))
